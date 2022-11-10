555130ecf77c6d6dec51f080666549ce3e0c6a95
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-10 19:55:28 +0100

    footer2

--- src/components/Footer2Comp.vue
+++ src/components/Footer2Comp.vue
@@ -1,10 +1,16 @@
 <template>
     <section>
         <div class="container">
-            <div>
-            </div>
+            <button><a href="#">Sign-Up Now!</a></button>
 
-            <div>
+            <div class="follow-us">
+                <h2>Follow us</h2>
+                <img src="../assets/img/footer-facebook.png" alt="Facebook" />
+                <img src="../assets/img/footer-twitter.png" alt="Twitter" />
+                <img src="../assets/img/footer-youtube.png" alt="YouTube" />
+                <img src="../assets/img/footer-pinterest.png" alt="Pinterest" />
+                <img src="../assets/img/footer-periscope.png" alt="Periscope" />
+
             </div>
         </div>
     </section>
@@ -23,13 +29,42 @@ export default {
 
 section {
     background-color: $footer2;
-    min-height: 7rem;
+    height: 7rem;
 
     .container {
+        min-height: 100%;
         padding: 2em;
         display: flex;
         justify-content: space-between;
         align-items: center;
+    }
+
+    button {
+        background-color: transparent;
+        border: 2px solid $lightblue;
+        padding: 1rem;
+        text-transform: uppercase;
+        font-weight: bold;
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+    }
+
+    .follow-us {
+        display: flex;
+        gap: 17px;
+
+        h2 {
+            text-transform: uppercase;
+            color: $lightblue;
+            margin: 0 0.5em;
+        }
+
+        img {
+            cursor: pointer;
+        }
     }
 
 
