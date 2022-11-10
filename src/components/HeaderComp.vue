6cf7d3e5c4e80add679f4aa8b7f1d0bf02fdb697
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-10 16:27:09 +0100

    add style in nav

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,15 +1,108 @@
 <template>
-    <header>
-        HEADER
+    <header class="container">
+        <a href="#">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </a>
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :href="link.url" :class="{ 'active': link.current }">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'HeaderComp'
+    name: 'HeaderComp',
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/mixins' as *;
+@use '../assets/styles/partials/variables' as *;
 
+header {
+    height: 7rem;
+    font-size: 400;
+    @include dflex;
+
+
+    img {
+        max-width: 70%;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        text-transform: uppercase;
+
+
+        li a {
+            padding: 1rem;
+            display: inline-block;
+            color: $black;
+            text-decoration: none;
+            font-weight: 600;
+
+            &.active,
+            &:hover {
+                border-bottom: 5px solid $lightblue;
+            }
+        }
+    }
+
+}
 </style>
