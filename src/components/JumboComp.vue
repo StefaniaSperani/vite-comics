f45bdea5cb9d14ad9f6e1a704f5247ed11fc70ee
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-11 14:17:42 +0100

    add button current series

--- src/components/JumboComp.vue
+++ src/components/JumboComp.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="jumbo">
-
+        <!-- IMG JUMBO -->
     </div>
+    <button class="series">Current Series</button>
 </template>
 
 <script>
@@ -10,9 +11,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../assets/styles/partials/mixins' as *;
+@use '../assets/styles/partials/variables' as *;
+
 .jumbo {
     background-image: url(../assets/img/jumbotron.jpg);
     min-height: 400px;
+    position: relative;
+}
+
+.series {
+    text-transform: uppercase;
+    padding: 13px;
+    background-color: $lightblue;
+    font-weight: bold;
+    color: #fff;
+    border: none;
+    font-size: 1.2em;
+    position: absolute;
+    bottom: 70px;
+    left: 300px;
 }
 </style>
